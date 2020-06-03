7d53c725e8b425968752c06089b8a9da0a62af31
renfrid <renfrid@2014>
2020-06-03 18:02:21 +0300

    Minor change on traveller\models.py

--- modules/travellers/models.py
+++ modules/travellers/models.py
@@ -60,8 +60,8 @@ class Symptom(models.Model):
 
 
 class DiseaseSymptom(models.Model):
-    disease_id = models.ForeignKey(Disease, on_delete=models.CASCADE)
-    symptom_id = models.ForeignKey(Symptom, on_delete=models.CASCADE)
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
+    symptom = models.ForeignKey(Symptom, on_delete=models.CASCADE)
     score = models.PositiveIntegerField()
 
     class Meta:
@@ -79,8 +79,8 @@ class Location(models.Model):
 
 
 class LocationDisease(models.Model):
-    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
-    disease_id = models.ForeignKey(Disease, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
     score = models.PositiveIntegerField()
 
     class Meta:
@@ -90,8 +90,8 @@ class LocationDisease(models.Model):
 # point of entries
 class PointOfEntry(models.Model):
     title = models.CharField(max_length=255)
-    region_id = models.PositiveIntegerField()  # to look around
-    district_id = models.PositiveIntegerField  # to look around
+    region = models.PositiveIntegerField()  # to look around
+    district = models.PositiveIntegerField  # to look around
     mode_of_transport = models.CharField(max_length=50)
 
     class Meta:
@@ -122,8 +122,8 @@ class Traveller(BaseModel):
 
     physical_address = models.TextField()
     hotel_name = models.CharField(max_length=255)
-    region_id = models.PositiveIntegerField  # to look around
-    district_id = models.PositiveIntegerField  # to look around
+    region = models.PositiveIntegerField  # to look around
+    district = models.PositiveIntegerField  # to look around
     street_or_ward = models.CharField(max_length=255)  # to look around
     phone = PhoneField(null=True, help_text='Contact Phone Number')
     email = models.EmailField(max_length=255)
@@ -152,69 +152,7 @@ class TravellerVisitedArea(models.Model):
 # Traveller symptoms
 class TravellerSymptoms(models.Model):
     traveller = models.ForeignKey(Traveller, on_delete=models.CASCADE)
-    symptom = models.CharField(max_length=50)
+    symptom = models.ForeignKey(Symptom, on_delete=models.DO_NOTHING)
 
     class Meta:
         db_table = "et_traveller_symptoms"
-
-
-# todo:secondary screening
-
-
-# class InternationalTraveller(BaseModel):
-#     full_name = models.CharField(max_length=250)
-#     sex = models.CharField(choices=SEX, max_length=15, default='none')
-#     age = models.PositiveIntegerField()
-#     passport_no = models.CharField(max_length=250)
-#     vehicle_flight_vehicle = models.CharField(max_length=250)
-#     nationality = models.CharField(max_length=150, default='Tanzania')  # to look around
-#     mode_of_transport = models.CharField(choices=TRANSPORT_MODE, max_length=30, default='none')  # to look around
-#     arrival_date = models.DateField(verbose_name="Arrival Date")
-#     seat_no = models.CharField(max_length=30)
-#     point_of_entry = models.CharField(max_length=250)  # to look around
-#
-#     visit_purpose = models.CharField(choices=PURPOSE, max_length=45, default='none')  # to look around
-#     other_purpose = models.TextField()
-#     duration_for_stay = models.PositiveIntegerField()
-#     employment = models.CharField(choices=EMPLOYMENT, max_length=45, default='none')
-#     other_employment = models.CharField(max_length=255)
-#
-#     physical_address = models.TextField()
-#     hotel_name = models.CharField(max_length=255)
-#     region = models.CharField(max_length=255)  # to look around
-#     district = models.CharField(max_length=255)  # to look around
-#     street_or_ward = models.CharField(max_length=255)  # to look around
-#     phone_number = PhoneField(null=True, help_text='Contact Phone Number')
-#     email_address = models.EmailField(max_length=255)
-#
-#     country_journey_started = models.CharField(max_length=250, default='Tanzania')  # to look around
-#
-#     class Meta(BaseModel.Meta):
-#         db_table = 'international_traveller'
-#
-#     def __str__(self):
-#         return self.full_name
-#
-#
-# class InternationalTravellerExt(BaseModel):
-#     international_traveller = models.ForeignKey(InternationalTraveller, on_delete=models.PROTECT, default=None)
-#     country = models.CharField(max_length=150, default='Tanzania')  # to look around
-#     location_visited = models.CharField(max_length=250)
-#     date = models.DateField(verbose_name="Date")
-#     days = models.PositiveIntegerField()
-#
-#     class Meta(BaseModel.Meta):
-#         db_table = 'international_traveller_ext'
-#
-#     def __str__(self):
-#         return f'{self.international_traveller}'
-#
-#
-# class TravellerSymptom(BaseModel):
-#     condition = models.ForeignKey(Conditions, on_delete=models.PROTECT, default=None)
-#
-#     class Meta(BaseModel.Meta):
-#         db_table = 'traveller_conditions'
-#
-#     def __str__(self):
-#         return f'{self.condition}'

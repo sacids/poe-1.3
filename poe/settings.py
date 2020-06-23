a959047dbeabea0e89c976e097857bce087b7cb0
renfrid <renfrid@2014>
2020-06-23 05:26:11 +0300

    Add server IP address

--- poe/settings.py
+++ poe/settings.py
@@ -26,9 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
-# ALLOWED_HOSTS = ['127.0.0.1','localhost','poe.sacids.org',]
-
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'poe.sacids.org', '206.81.14.150']
 
 # Application definition
 

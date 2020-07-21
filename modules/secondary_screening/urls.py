d8708e0ada96332f57c10645208891d33cb268be
Eric Beda <ericbeda@gmail.com>
2020-07-21 12:17:05 +0300

    minor changes

--- modules/secondary_screening/urls.py
+++ modules/secondary_screening/urls.py
@@ -4,8 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', screen_list, name='sec_screen'),
-    #re_path(r'^survey/(?P<disease_id>\d+)/$', survey, name='survey'),
-    #re_path(r'^manage/(?P<travellers_id>\d+)/$', screen_traveller, name='manage'),
     path('<int:travellers_id>/action/', risk_assessment, name='risk_assessment'),
     path('<int:travellers_id>/info/', traveller_info, name='traveller_info'),
     path('<int:travellers_id>/', screen_traveller, name='screen_traveller'),

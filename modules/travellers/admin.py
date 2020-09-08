254253f4f6fe6472d0c43e302c0ae8945849651a
Renfrid Ngolongolo <renfrid@Renfrids-MacBook-Pro.local>
2020-09-08 16:44:39 +0300

    Modify change on django-admin

--- modules/travellers/admin.py
+++ modules/travellers/admin.py
@@ -3,29 +3,33 @@ from .models import *
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ['title', 'code',]
+    list_display = ['id','title', 'code', 'parent']
     search_fields = ['title__startwith']
+    ordering = ("id",)
 
 @admin.register(PointOfEntry)
 class PointOfEntryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'mode_of_transport']
+    list_display = ['id','title', 'mode_of_transport']
     list_filter = ['mode_of_transport']
     search_fields = ['title__startwith']
+    ordering = ("id",)
 
 class ScreenCriteriaInline(admin.StackedInline):
     model = ScreenCriteria
 
 @admin.register(Disease)
 class DiseaseAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['id','title']
     ordering = ('title',)
     search_fields = ("title__startswith", )
+    ordering = ("id",)
     inlines  = [ScreenCriteriaInline]
 
 @admin.register(Symptom)
 class SymptomAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['id','title']
     ordering = ('title',)
+    ordering = ("id",)
     search_fields = ['title__startwith']
 
 

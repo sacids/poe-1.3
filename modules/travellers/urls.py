caee8d4c1edde2c32813bbe2442cc4705f50614f
Renfrid Ngolongolo <renfrid@Renfrids-MacBook-Pro.local>
2020-09-15 14:32:53 +0300

    Create Action Taken model

--- modules/travellers/urls.py
+++ modules/travellers/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from . import views
-
-from .views import default, change_language_en, change_language_sw, international, domestic, success, auto_districts, auto_point_of_entries
+from .views import *
 
 app_name = 'travellers'
 urlpatterns = [
     path('', default, name='index'),
     path('change_language_en', views.change_language_en, name='change_language_en'),
     path('change_language_sw', views.change_language_sw, name='change_language_sw'),
-    path('international/', international, name='international'),
-    path('domestic/', domestic, name='domestic'),
+    path('arrival/', arrival, name='arrival'),
+    path('departure/', departure, name='departure'),
     path('success/', success, name='success'),
     path('auto_districts/', auto_districts, name='auto_districts'),
     path('auto_point_of_entries/', auto_point_of_entries, name='auto_point_of_entries')

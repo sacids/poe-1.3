a3271510254de398bd3be9e208afec0259f1cebc
mchizani <ericbeda@gmail.com>
2020-09-30 10:52:47 +0300

    manage user data on screen view

--- modules/common/templatetags/poe_tags.py
+++ modules/common/templatetags/poe_tags.py
@@ -1,6 +1,11 @@
 from django import template
 import json
 
+from django.utils.safestring import mark_safe
+from django.template.defaultfilters import stringfilter
+from django.utils.text import normalize_newlines
+
+
 register    = template.Library()
 
 @register.filter(name='replace_underscore')
@@ -8,3 +13,15 @@ def replace_underscore(string):
     return string.replace('_', ' ')
 
 
+def remove_newlines(text):
+    """
+    Removes all newline characters from a block of text.
+    """
+    # First normalize the newlines using Django's nifty utility
+    normalized_text = normalize_newlines(text)
+    # Then simply remove the newlines like so.
+    return mark_safe(normalized_text.replace('\n', ' '))
+    
+remove_newlines.is_safe = True
+remove_newlines = stringfilter(remove_newlines)
+register.filter(remove_newlines)

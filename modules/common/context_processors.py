efc208ac75254ff4644243e7635c9af45d87c7a8
mchizani <ericbeda@gmail.com>
2020-10-07 08:57:11 +0300

    line list dependent of POE chosen

--- modules/common/context_processors.py
+++ modules/common/context_processors.py
@@ -3,12 +3,19 @@ from modules.travellers.models import PointOfEntry
 
 
 def poe_defaults(request):
+
+    poes    = PointOfEntry.objects.filter(agents__id=request.user.id).order_by('id')
+    
+    default_poe     = -1
+    if poes is not None:
+        default_poe     = poes[0]
+    
     context = {
         'search': True,
         'login': True,
         "modules": Module.objects.all(),
-        'poes': PointOfEntry.objects.filter(agents__id=request.user.id),
-        'poe_id': request.session.get('poe_id', 0),
+        'poes': poes,
+        'poe_id': request.session.get('poe_id', default_poe),
         'poe_title': request.session.get('poe_title', 'ALL'),
     }
 
